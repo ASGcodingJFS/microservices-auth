e9aa354be012812f2e508d4ff9e80618656be0c0
ASGCodingJFS <arnabsengupta.fsdjr@gmail.com>
2025-12-20 22:53:17 +0530

    Initial commit

---
+++ api-agteway/src/main/java/com/api_agteway/ApiAgtewayApplication.java
@@ -0,0 +1,13 @@
+package com.api_agteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgtewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgtewayApplication.class, args);
+	}
+
+}
---
+++ api-agteway/src/test/java/com/api_agteway/ApiAgtewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_agteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgtewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
